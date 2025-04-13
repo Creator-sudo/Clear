31e962de8ad4926f4176abe270995664eff0b9ed
Creator-sudo <shinchau119@gmail.com>
2025-04-13 12:54:12 +0530

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -4,19 +4,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Setting up background recording automatically when app is launched
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-                // Start the recording immediately
-                if let rootViewController = appDelegate.window?.rootViewController as? ViewController {
-                    rootViewController.startRecording()
-                }
-            }
-            // Move app to background
-            UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
-        }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Automatically send the app to the background
+        application.beginBackgroundTask(expirationHandler: nil)
     }
 }
